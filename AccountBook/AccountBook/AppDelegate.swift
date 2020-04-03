d56c0b39ed6980511301c7bf180cceb78b63bbb7
seabrea <hgdigm@gmail.com>
2020-04-04 01:45:08 +0800

    创建项目

---
+++ AccountBook/AccountBook/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AccountBook
+//
+//  Created by He Bob on 2020/4/2.
+//  Copyright © 2020 He Bob. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AccountBook/AccountBook/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AccountBook
+//
+//  Created by He Bob on 2020/4/2.
+//  Copyright © 2020 He Bob. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
